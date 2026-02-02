91028d249f53fa8f56bd6acb4f37334f9bfd34f4
wns <wnsoff@yandex.com>
2026-02-02 12:18:03 +0300

    add config files

---
+++ app/config.py
@@ -0,0 +1,16 @@
+import os
+import dotenv
+
+dotenv.load_dotenv()
+
+
+class Config:
+	DB_USER: str = os.getenv("DB_USER")
+	DB_PASS: str = os.getenv("DB_PASS")
+	DB_HOST: str = os.getenv("DB_HOST")
+	DB_PORT: str = os.getenv("DB_PORT")
+	DB_NAME: str = os.getenv("DB_NAME")
+
+	@classmethod
+	def get_database_url(cls):
+		return f"postgresql+asyncpg://{cls.DB_USER}:{cls.DB_PASS}@{cls.DB_HOST}:{cls.DB_PORT}/{cls.DB_NAME}"
