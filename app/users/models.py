ad4b13519f5e68f8682fd68be8f99e400165e189
wns <wnsoff@yandex.com>
2026-02-09 00:11:29 +0300

    feat: add realetionship

--- app/users/models.py
+++ app/users/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Boolean, Column, DateTime, Integer, String
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 from app.database import Base
 
 
@@ -15,3 +16,5 @@ class User(Base):
 
 	created_at = Column(DateTime(timezone=True), server_default=func.now())
 	updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+	comments = relationship("Comment", back_populates="user")
