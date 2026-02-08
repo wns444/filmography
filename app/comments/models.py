e9ad6eb1194ea5cb3dbe74f89a1b345f38ff90ea
wns <wnsoff@yandex.com>
2026-02-08 23:57:13 +0300

    feat: content types

--- app/comments/models.py
+++ app/comments/models.py
@@ -1,7 +1,13 @@
+import enum
 from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
 from app.database import Base
+
+
+class ContentType(enum.IntEnum):
+	FILM = 1
+	CHAPTER = 2
 
 
 class Comment(Base):
@@ -11,10 +17,11 @@ class Comment(Base):
 	id = Column(Integer, primary_key=True, autoincrement=True)
 	user_id = Column(ForeignKey("users.id"), nullable=False)
 	text = Column(String, nullable=False)
+	content_type = Column(Integer, nullable=False)
+	content_id = Column(Integer, nullable=False)
 	parent_id = Column(Integer, ForeignKey('comments.id'), nullable=True)
 
-	children = relationship("Comment", backref="parent", remote_side=[id])
-	user = relationship("User", back_populates="users")
+	user = relationship("User", back_populates="comments")
 
 	created_at = Column(DateTime(timezone=True), server_default=func.now())
 	updated_at = Column(DateTime(timezone=True), onupdate=func.now())
