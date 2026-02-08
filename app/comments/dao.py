7d526e0bb11250e43aa00440e66188fa56597628
wns <wnsoff@yandex.com>
2026-02-08 23:57:43 +0300

    feat: new schemas and dao

--- app/comments/dao.py
+++ app/comments/dao.py
@@ -1,6 +1,30 @@
-from app.comments.models import Comment
+from typing import Union
+
+from sqlalchemy import select
+from app.comments.models import Comment, ContentType
 from app.dao.base import BaseDAO
+from app.database import async_session_maker
+from app.users.models import User
 
 
 class CommentDAO(BaseDAO):
 	model = Comment
+
+	@classmethod
+	async def get_all_comments(cls, content_type: Union[ContentType.FILM, ContentType.CHAPTER], content_id: int):
+		async with async_session_maker() as session:
+			query = (
+				select(cls.model, User.username, User.email)
+				.where(
+					cls.model.content_type == content_type,
+					cls.model.content_id == content_id
+				)
+				.join(User, cls.model.user_id == User.id)
+			)
+			result = await session.execute(query)
+			comments_with_user = []
+			for comment, username, email in result:
+				comment.user_info = {"username": username, "email": email}  # Добавляем как атрибут
+				comments_with_user.append(comment)
+
+			return comments_with_user
--- app/comments/schemas.py
+++ app/comments/schemas.py
@@ -1,8 +1,30 @@
 from typing import Optional
 from pydantic import BaseModel
+from datetime import datetime
 
 
-class CommentS(BaseModel):
+# Pydantic v2 ORM support
+class ConfigModel(BaseModel):
+	model_config = {"from_attributes": True}
+
+
+class CommentS(ConfigModel):
+	user_id: int
+	text: str
+	parent_id: Optional[int] = None
+
+
+class CommentUpdate(ConfigModel):
+	text: Optional[str]
+	parent_id: Optional[int]
+
+
+class CommentOut(ConfigModel):
+	id: int
 	user_id: int
 	text: str
 	parent_id: Optional[int]
+	content_type: int
+	content_id: int
+	created_at: Optional[datetime]
+	updated_at: Optional[datetime]
