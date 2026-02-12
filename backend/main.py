10a0525450f85c9ba03af46282f13582af3e0722
wns <wnsoff@yandex.com>
2026-02-13 01:54:51 +0300

    feat: add middleware

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.api.router import router as router_api
 from backend.users.router import router as router_user
@@ -8,6 +9,20 @@ app = FastAPI()
 
 app.include_router(router_api)
 app.include_router(router_user)
+
+origins = [
+	"http://localhost:3000",
+	"http://0.0.0.0:3000",
+	"http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=origins,
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"],
+)
 
 
 @app.get("/")
